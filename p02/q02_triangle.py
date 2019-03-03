502d0718cd98150217dbccd3c351ba8910ae5bca
Liam-Jaedick <47766329+Liam-Jaedick@users.noreply.github.com>
2019-03-03 21:37:36 +0800

    Update q02_triangle.py

--- p02/q02_triangle.py
+++ p02/q02_triangle.py
@@ -1,8 +1,8 @@
-s1 = int(input("Enter side 1: "))
-s2 = int(input("Enter side 2: "))
-s3 = int(input("Enter side 3: "))
+s1 = int(input("Enter side 1(cm): "))
+s2 = int(input("Enter side 2(cm): "))
+s3 = int(input("Enter side 3(cm): "))
 
-if s1 >= s2 + s3 or s2 >= s1 + s3 or s3 >= s1 + s2
+if s1 >= s2 + s3 or s2 >= s1 + s3 or s3 >= s1 + s2:
     print("Invalid triangle!")
 else: 
-    print(str(s1 + s2 + s3))
+    print("Perimeter is " + str(s1 + s2 + s3) + "cm")
