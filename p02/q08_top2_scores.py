478cb8249d0b481f5e5955125fdb113206d7d905
Liam-Jaedick <47766329+Liam-Jaedick@users.noreply.github.com>
2019-03-04 01:17:52 +0800

    Update q08_top2_scores.py

--- p02/q08_top2_scores.py
+++ p02/q08_top2_scores.py
@@ -5,12 +5,11 @@ def grades(name, score):
 num_student = int(input("Number of Students: "))
 
 x = 0
-
 while x < num_student:
     n = input("Name: ")
     s = input("Score:")
     grades(n,s)
-    x = x+1
+    x = x + 1
 
 print(max(scores))
 scores.pop(max(scores))
