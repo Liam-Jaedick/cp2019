1a4e5484513d1942db136a1d87881d9c0e788996
Liam-Jaedick <47766329+Liam-Jaedick@users.noreply.github.com>
2019-03-03 21:21:10 +0800

    Create q02_triangle.py

---
+++ p02/q02_triangle.py
@@ -0,0 +1,8 @@
+s1 = int(input("Enter side 1: "))
+s2 = int(input("Enter side 2: "))
+s3 = int(input("Enter side 3: "))
+
+if s1>=s2+s3 or s2>=s1+s3:
+    print("Invalid triangle!")
+else: 
+    print(str(s1+s2+s3))
