8748939558ab4856ec0e6c060b3648a7f8c222fc
Liam-Jaedick <47766329+Liam-Jaedick@users.noreply.github.com>
2019-03-03 21:24:03 +0800

    Create q07_miles_to_kilometres.py

---
+++ p02/q07_miles_to_kilometres.py
@@ -0,0 +1,8 @@
+def mtokm(dist):
+    km = dist*1.609
+    return "{:.3f}".format(km)
+
+print("Miles","Kilometers","Kilometres","Miles")
+
+for i in range(1,11):
+    print(str(i),'   ',mtokm(i),"    ",str(5*i+20),"       ",mtokm(5*i+20))
