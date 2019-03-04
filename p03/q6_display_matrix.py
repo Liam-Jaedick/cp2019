8a901c192d6c402cca3f6a156fd38f6863fbb4af
Liam-Jaedick <47766329+Liam-Jaedick@users.noreply.github.com>
2019-03-05 00:33:20 +0800

    Update q6_display_matrix.py

--- p03/q6_display_matrix.py
+++ p03/q6_display_matrix.py
@@ -1,2 +1,10 @@
-def print_matrix(n)
-  
+from random import randint
+
+def print_matrix(n):
+    for i in range(n):
+        for i in range(n):
+            print(f"{randint(0,1)}",end=" ")
+        print()
+
+n = int(input("Enter Number: "))
+print_matrix(n)
